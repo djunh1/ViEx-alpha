1167bfe0944a2fb2501c1d11d65508667f84ecdb
djunh1 <djunh1@gmail.com>
2015-12-25 08:53:55 -0500

    Home page view returns html

--- viex/tests.py
+++ viex/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
+
 from viex.views import home_page
 
 # Create your tests here.
@@ -7,4 +10,11 @@ class HomePageTest(TestCase):
 
 	def test_root_url_resolves_to_homepage_view(self):
 		found=resolve('/')
-		self.assertEqual(found.func,home_page)
+		self.assertEqual(found.func, home_page)
+
+	def test_homepage_returns_correct_html(self):
+		request=HttpRequest()
+		response=home_page(request)
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
