98efcc06edc2b4e3e06ade002ee8d0cfcc05ba1b
djunh1 <djunh1@gmail.com>
2015-12-24 21:17:50 -0500

    Unit test and url mapping to dummy view

--- superlists/urls.py
+++ superlists/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import url
+
+from viex import views
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',views.home_page,name='home'),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
