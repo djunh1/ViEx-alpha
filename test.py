25132e8e1c442cb51d8e931d3bebc8cddf5c89cb
djunh1 <djunh1@gmail.com>
2015-12-27 11:01:16 -0500

    functional test checks input of stock ticker

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -15,8 +16,26 @@ class NewVisitorTest(unittest.TestCase):
         # Check to see if you can get into the website
         self.browser.get('http://localhost:8000')
 
-        # She notices the page title and header mention to-do lists
-        #self.assertIn('To-Do', self.browser.title)  
+        # Look at browswer title 
+
+        self.assertIn('Value Investing Exchange',self.browser.title)
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Value Investing Exchange',header_text)
+
+        #Enter an item into the text box
+        inputbox=self.browser.find_element_by_id('id_stock_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a stock')
+
+        #Type NOV into a text box
+        inputbox.send_keys('NOV')
+
+        #When entered, NOV should appear on the sceen
+        inputbox.send_keys(keys.ENTER)
+
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text=='1: NOV' for row in rows))
+
         self.fail('Finish the test!')  
 
         #Other comments
